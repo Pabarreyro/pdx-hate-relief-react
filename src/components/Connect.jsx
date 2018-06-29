5db69df28ef55a48b68ea22fac70da97354e54ab
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-29 16:05:53 -0700

    Add component

---
+++ src/components/Connect.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Learn() {
+  return(
+    <h1>Connect with local reaources!</h1>
+  );
+}
+export default Learn;
---
+++ src/components/Learn.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Learn() {
+  return(
+    <h1>Learn about PUAH!</h1>
+  );
+}
+export default Learn;
