91622c2fae6581e21d9ec2b733184f99699f2deb
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-29 16:06:13 -0700

    Add Switch and route paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Header from "./Header";
+import Home from './Home';
+import Learn from './Learn';
+import Report from './Report';
+import Connect from './Connect';
 
 function App(){
   return (
     <div>
       <Header/>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/learn' component={Learn} />
+        <Route path='/report' component={Report} />
+        <Route path='/connect' component={Connect} />
+      </Switch>
     </div>
   );
 }
