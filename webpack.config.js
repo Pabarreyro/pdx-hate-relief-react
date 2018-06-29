ca3ebf809e178bec8de397cc5b1bac1ef348e783
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-29 14:35:10 -0700

    Configure url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,16 @@ module.exports = {
           ]
         }
       },
+      {
+      test: /\.(png|gif|jp(e*)g|svg)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8000,
+          name: 'images/[hash]-[name].[ext]'
+        }
+      }
+    }
     ],
   },
   plugins: [
