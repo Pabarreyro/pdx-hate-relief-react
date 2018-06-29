d90c080244b703d2add717e56a20cb7e1687ff1c
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-29 15:06:05 -0700

    Update component to bootstrap navBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import cccLogo from '../assets/images/ccc.png';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 function Header(){
   return (
-    <div>
-      <img src={cccLogo}/>
-    </div>
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <img src={cccLogo}/>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          Report
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          Connect
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 }
 
