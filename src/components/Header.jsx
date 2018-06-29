f753f56be335b0c427984ee15e301fcd3f598fee
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-29 14:45:17 -0700

    Add CCC logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import cccLogo from '../assets/images/ccc.png';
+
+function Header(){
+  return (
+    <div>
+      <img src={cccLogo}/>
+    </div>
+  );
+}
+
+export default Header;
