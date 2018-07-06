e144bca1fe4f32d0721e4ad1a018f53f1873f7f6
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-06 15:41:44 -0700

    Refactor to be stateful

--- src/components/ResourceFilter.jsx
+++ src/components/ResourceFilter.jsx
@@ -1,72 +1,77 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { Jumbotron, Button } from 'react-bootstrap';
 import Select from 'react-select';
 
-function ResourceFilter(props) {
-  return(
-    <div>
-      <style jsx>
-        {`
-          Jumbotron {
-            text-align: center;
-          }
-        `}
-      </style>
-      <Jumbotron>
-        <div>
-          <h1>Helping Portland heal from hate</h1>
-          <p>Connect with organizations across Potland who are helping communities heal from and prevent hate crimes.</p>
-          <div className="resource-search">
-            <div>
-              <div class="col-md-3">
-                <label for="communities"> I am </label>
-                <Select
-                  name="communities"
-                  value={props.selectedCommunities}
-                  onChange={onSelectCommunities}
-                  options={props.communities} />
-                </Select>
+class ResourceFilter extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCommunities: '',
+      selectedServices: '',
+      selectedRegions: '',
+      regions: [],
+      communities: [],
+      services: []
+    }
+  }
+
+  render() {
+    const { selectedCommunities } = this.state;
+    const { selectedServices } = this.state;
+    const { selectedRegions } = this.state;
+    
+    return(
+      <div>
+        <style jsx>
+          {`
+            Jumbotron {
+              text-align: center;
+            }
+          `}
+        </style>
+        <Jumbotron>
+          <div>
+            <h1>Helping Portland heal from hate</h1>
+            <p>Connect with organizations across Potland who are helping communities heal from and prevent hate crimes.</p>
+            <div className="resource-search">
+              <div>
+                <div class="col-md-3">
+                  <label for="communities"> I am </label>
+                  <Select
+                    name="communities"
+                    value={props.selectedCommunities}
+                    onChange={onSelectCommunities}
+                    options={props.communities} />
+                  </Select>
+                </div>
+                <div class="col-md-3">
+                  <label for="services">Looking for</label>
+                  <Select
+                    [name="services"
+                    value={props.selectedServices}
+                    onChange={onSelectServices}
+                    options={props.services} />
+                  </Select>
+                </div>
+                <div class="col-md-3">
+                  <label for="regions">In</label>
+                  <Select
+                    name="regions"
+                    value={props.selectedRegions}
+                    onChange={onSelectRegions}
+                    options={props.Regions} />
+                  </Select>
+                </div>
+                <Button>Find Resources</Button>
               </div>
-              <div class="col-md-3">
-                <label for="services">Looking for</label>
-                <Select
-                  [name="services"
-                  value={props.selectedServices}
-                  onChange={onSelectServices}
-                  options={props.services} />
-                </Select>
-              </div>
-              <div class="col-md-3">
-                <label for="regions">In</label>
-                <Select
-                  name="regions"
-                  value={props.selectedRegions}
-                  onChange={onSelectRegions}
-                  options={props.Regions} />
-                </Select>
-              </div>
-              <Button>Find Resources</Button>
             </div>
           </div>
-        </div>
-      </Jumbotron>
-    </div>
-
-  )
-}
-
-ResourceFilter.propTypes = {
-    regions = PropTypes.array.isRequired,
-    services = PropTypes.array.isRequired,
-    communities = PropTypes.array.isRequired,
-    selectedCommunities = PropTypes.string,
-    selectedServices = PropTypes.string,
-    selectedRegions = PropTypes.string,
-    onSelectCommunities = PropTypes.func,
-    onSelectServices = PropTypes.func,
-    onSelectRegions = PropTypes.func
+        </Jumbotron>
+      </div>
+    )
+  }
 }
 
 export default ResourceFilter;
