434402aff688f000562d5417a513b1f0e3b80678
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-06 14:38:44 -0700

    Import and declare multi-select and resource props

--- src/components/ConnectControl.jsx
+++ src/components/ConnectControl.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ConnectControl() {
   return(
     <h1>Connect with local reaources!</h1>
   );
+}
+
+ConnectControl.propTypes = {
+  resources: PropTypes.object,
+  services: PropTypes.array,
+  communities: PropTypes.array, 
+  regions: PropTypes.array
 }
 export default ConnectControl;
