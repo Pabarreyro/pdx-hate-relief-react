2fe7a19d452c630e1ad894363d15cbcecadd35ac
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-06 17:17:58 -0700

    Center form labels

--- src/components/ResourceFilter.jsx
+++ src/components/ResourceFilter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron, Row, Col, Button } from 'react-bootstrap';
 import Select from 'react-select';
 import '!style-loader!css-loader!react-select/dist/react-select.css';
 
@@ -11,46 +11,60 @@ function ResourceFilter(props) {
     <div>
       <style jsx>
         {`
-          Jumbotron {
+          .connect-header {
+            text-align: center;
+          }
+
+          .form-label {
             text-align: center;
           }
         `}
       </style>
       <Jumbotron>
         <div>
-          <h1>Helping Portland heal from hate</h1>
-          <p>Connect with organizations across Potland who are helping communities heal from and prevent hate crimes.</p>
-          <div className="resource-search">
-            <div>
-              <div md={3}>
-                <label for="communities"> I am </label>
+          <div className="connect-header">
+            <h1>Helping Portland heal from hate</h1>
+            <p>Connect with organizations across Potland who are helping communities heal from and prevent hate crimes.</p>
+          </div>
+          <div>
+            <Row>
+              <Col md={3}>
+                <div className="form-label">
+                  <label for="communities"> I am </label>
+                </div>
                 <Select
                   name="communities"
                   value={props.selectedCommunities}
                   onChange={props.onSelectCommunities}
                   options={props.communities}>
                 </Select>
-              </div>
-              <div md={3}>
-                <label for="services">Looking for</label>
+              </Col>
+              <Col md={3}>
+                <div className="form-label">
+                  <label for="services">Looking for</label>
+                </div>
                 <Select
                   name="services"
                   value={props.selectedServices}
                   onChange={props.onSelectServices}
                   options={props.services}>
                 </Select>
-              </div>
-              <div md={3}>
-                <label for="regions">In</label>
+              </Col>
+              <Col md={3}>
+                <div className="form-label">
+                  <label for="regions">In</label>
+                </div>
                 <Select
                   name="regions"
                   value={props.selectedRegions}
                   onChange={props.onSelectRegions}
                   options={props.regions}>
                 </Select>
-              </div>
-              <Button>Find Resources</Button>
-            </div>
+              </Col>
+              <Col md={3}>
+                <Button>Find Resources</Button>
+              </Col>
+            </Row>
           </div>
         </div>
       </Jumbotron>
