35978e8150b31f81553b381f11acbc0ad1c4469f
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-06 17:17:28 -0700

    Add component placeholder

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ResourceCard(props) {
-
+  return <h1>I'm a Resource Card!</h1>
 }
 
 ResourceCard.propTypes = {
-  
+
 };
+
 export default ResourceCard;
