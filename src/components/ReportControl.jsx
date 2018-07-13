07a388eb150ba60e3681e32e4d7aed0016945787
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-13 15:55:20 -0700

    Refactor conditional rendering using ternary operators

--- src/components/ReportControl.jsx
+++ src/components/ReportControl.jsx
@@ -3,16 +3,13 @@ import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
 function ReportControl(props) {
-  let currentView = null;
 
-  if (!props.newReport) {
-    currentView = <Button onClick={props.onStartReport}>Start Report</Button>;
-  } else {
-    currentView = <h1>Continue report</h1>;
-  }
   return(
     <div>
-      {currentView}
+      { props.newReport ?
+        <h1>Continue report</h1>
+        : <Button onClick={props.onStartReport}>Start Report</Button>
+      }
     </div>
   );
 }
