cf7fb4abe83e1107d869335e43c22efa0c081f19
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-13 16:18:22 -0700

    Add conditional logic to advance through report sections

--- src/components/ReportControl.jsx
+++ src/components/ReportControl.jsx
@@ -7,7 +7,31 @@ function ReportControl(props) {
   return(
     <div>
       { props.newReport ?
-        <h1>Continue report</h1>
+        (props.currentReportSection === 1) ? (
+            <div>
+              <h1>About Me</h1>
+              <Button onClick={props.onAdvanceSection}>Save & Continue</Button>
+            </div>
+          ) :
+        (props.currentReportSection === 2) ? (
+            <div>
+              <h1>Time + Place</h1>
+              <Button onClick={props.onAdvanceSection}>Save & Continue</Button>
+            </div>
+          ) :
+        (props.currentReportSection === 3) ? (
+            <div>
+              <h1>Incident Details</h1>
+              <Button onClick={props.onAdvanceSection}>Save & Continue</Button>
+            </div>
+          ) :
+        (props.currentReportSection === 4) ? (
+            <div>
+              <h1>Additional Info</h1>
+              <Button onClick={props.onAdvanceSection}>Save & Continue</Button>
+            </div>
+          ) :
+        <h1>Review</h1>
         : <Button onClick={props.onStartReport}>Start Report</Button>
       }
     </div>
@@ -15,8 +39,10 @@ function ReportControl(props) {
 }
 
 ReportControl.propTypes = {
-  newReport : PropTypes.boolean,
-  onStartReport : PropTypes.func
+  newReport : PropTypes.bool,
+  currentReportSection: PropTypes.number,
+  onStartReport : PropTypes.func,
+  onAdvanceSection : PropTypes.func
 };
 
 export default ReportControl;
