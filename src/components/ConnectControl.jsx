70ec321afdb60eb33f639b8158187c1a3a04ed33
Pablo Barreyro <Pabarreyro@gmail.com>
2018-07-13 13:15:42 -0700

    Add conditional rendering for resource output

--- src/components/ConnectControl.jsx
+++ src/components/ConnectControl.jsx
@@ -13,7 +13,23 @@ function ConnectControl(props) {
         onSelectCommunities={props.onSelectCommunities}
         onSelectServices={props.onSelectServices}
         onSelectRegions={props.onSelectRegions}
-        onSubmitFilters={this.handleSubmitFilters}/>
+        onSubmitFilters={props.onSubmitFilters} />
+      <div>
+        {
+          props.defaultResources ? (
+            <h4>Try Selecting search critera</h4>
+          ) : (
+            <div>
+              <h1>Great! I see you've selected the following filter criteria:</h1>
+              <h3><strong>Services: </strong>{props.selectedServices}</h3>
+              <h3><strong>Communities: </strong>{props.selectedCommunities}</h3>
+              <h3><strong>Regions: </strong>{props.selectedRegions}</h3>
+              <hr/>
+              <ResourceCard />
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
@@ -29,6 +45,7 @@ ConnectControl.propTypes = {
   onSelectCommunities: PropTypes.func,
   onSelectServices: PropTypes.func,
   onSelectRegions: PropTypes.func,
-  onSubmitFilters: PropTypes.func
+  onSubmitFilters: PropTypes.func,
+  defaultResources: PropTypes.bool
 };
 export default ConnectControl;
